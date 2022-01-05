90edf4c4cf227f9500eae50cd3132009573e139e
Артем Вафин <a.vafin85@gmail.com>
2022-01-05 23:38:50 +0500

    add gulp html

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ const postcss = require(`gulp-postcss`);
 const autoprefixer = require(`autoprefixer`);
 const csso = require(`gulp-csso`);
 const rename = require(`gulp-rename`);
+const fileInclude = require(`gulp-file-include`);
+const typograf = require("gulp-typograf");
 
 const styles = () => {
   return gulp
@@ -18,9 +20,28 @@ const styles = () => {
     .pipe(postcss([autoprefixer()]))
     .pipe(csso())
     .pipe(rename(`styles.min.css`))
-    .pipe(gulp.dest(`build/css`, { sourcemaps: `.` }));
+    .pipe(gulp.dest(`./build/css`, { sourcemaps: `.` }));
 };
 
 exports.styles = styles;
 
-exports.default = styles;
+const html = () => {
+  return gulp
+    .src(`source/html/**/*.html`)
+    .pipe(
+      fileInclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(
+      typograf({
+        locale: ["ru", "en-US"],
+      })
+    )
+    .pipe(gulp.dest(`./build`));
+};
+
+exports.html = html;
+
+exports.default = gulp.series(styles, html);
